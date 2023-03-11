25248bd3065e9fd15f463ec70cdd730a708d6ae0
[David <[nmueller855@gmail.com]>
2023-03-11 11:45:29 -0800

    basic outline of the component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='remote'>
+        <div className='clock'>
+          <div className='countDownNumber'>5:00</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
