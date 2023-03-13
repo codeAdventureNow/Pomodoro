a70e52c3d9afc89f9f59e9e8d208fae3473cce94
[David <[nmueller855@gmail.com]>
2023-03-13 13:23:50 -0700

    able to get the stopwatch to countdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [time, setTime] = useState(60000);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime - 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
   return (
     <div className='App'>
       <div className='remote'>
         <div className='clock'>
-          <div className='countDownNumber'>5:00</div>
+          <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+          <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}</span>
         </div>
+        <button onClick={() => setRunning(true)}>Start</button>
+        <button onClick={() => setRunning(false)}>Stop</button>
+        <button onClick={() => setTime(1190000)}>Reset</button>
       </div>
     </div>
   );
